4a9fc2c7fc19f0f5f7f319544412394cb69414da
Miguel Ballen <mballen@mcclatchyservices.com>
2021-01-06 21:03:17 -0500

    QUASAR - Nivel 1

--- main.go
+++ main.go
@@ -2,6 +2,30 @@ package main
 
 import "fmt"
 
+type satellite struct {
+	x float32
+	y float32
+}
+
+func newSatellite(x float32, y float32) *satellite {
+	s := satellite{x: x, y: y}
+	return &s
+}
+
+func (sat satellite) GetLocation(distance float32) (x, y float32) {
+	return sat.x, sat.y
+}
+
+func getMessage(messages []string) (msg string) {
+	return messages[0]
+}
+
 func main() {
-	fmt.Println("Hello, world.")
+	kenobi := newSatellite(-500, 200)
+	skywalker := newSatellite(100, -100)
+	sato := newSatellite(500, 100)
+
+	fmt.Println(kenobi)
+	fmt.Println(skywalker)
+	fmt.Println(sato)
 }
